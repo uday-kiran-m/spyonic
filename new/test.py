e7ea162e705fd7323c73a20d472a5b950925a6c2
uday-kiran-m <m.udaykiran499569@gmail.com>
2022-03-01 15:05:27 +0530

    test

--- new/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-print(sys.argv)
-
-import os
-print('Get current working directory :      ', os.getcwd())
-print('Get current file name :    ', __file__)
-
-print('File name :    ', os.path.basename(__file__))
-print('Directory Name:     ', os.path.dirname(__file__))
