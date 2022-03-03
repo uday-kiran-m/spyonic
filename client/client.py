ae2c857df0015c3ee0689c5ea751b0763f07bc8c
uday-kiran-m <m.udaykiran499569@gmail.com>
2022-03-03 19:43:18 +0530

    test

--- client/client.py
+++ client/client.py
@@ -2,15 +2,15 @@ from mod import client
 import eel
 
 # email = input("Enter email: ")
-email = 'test'
+email = 'xyz123@gmail.com'
 # ip = input('Enter server IP:')
 ip = '25.41.20.120'
 cli = client(ip)
 if not cli.is_installed():
-    cli.register(email,'123')
+    cli.register(email,'0987654321')
     # cli.start()
 else:
-    x = cli.login(email,'123')
+    x = cli.login(email,'0987654321')
     if x == True:
         cli.start()
     else:
